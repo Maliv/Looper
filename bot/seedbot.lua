80376490daf40f0a27c95d8bef0137b3b7462e0f
Maliv <ma.vafaei.2002@gmail.com>
2016-02-09 21:31:46 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -229,7 +229,23 @@ function create_config( )
     sudo_users = {100610521,95149435},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
-    about_text = {Teleseed No   Its Looper},
+    about_text = [[Teleseed v2 - Open Source
+An advance Administration bot based on yagop/telegram-bot 
+https://github.com/SEEDTEAM/TeleSeed
+Admins
+@iwals [Founder]
+@imandaneshi [Developer]
+@Rondoozle [Developer]
+@seyedan25 [Manager]
+Special thanks to
+awkward_potato
+Siyanew
+topkecleon
+Vamptacus
+Our channels
+@teleseedch [English]
+@iranseed [persian]
+]],
     help_text_realm = []
 Realm Commands:
 
